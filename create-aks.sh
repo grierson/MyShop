61dbb339d2c6f6f89c6da6a90fdf0887d753cc84
Kyle Grierson <griersonky@gmail.com>
2021-09-03 13:50:06 +0100

    Run Kubernetes locally

--- create-aks.sh
+++
@@ -1,9 +0,0 @@
-az group create --name MicroservicesInDotnet --location northeurope
-
-az acr create --resource-group MicroservicesInDotnet --name comgriersonmsdn --sku Basic
-
-az aks create --resource-group MicroservicesInDotnet --name MicroservicesInDotnetAKSCluster --node-count 1 --enable-addons monitoring --generate-ssh-keys --attach-acr comgriersonmsdn
-
-az aks get-credentials --resource-group MicroservicesInDotnet --name MicroservicesInDotnetAKSCluster
-
-kubectl get nodes
